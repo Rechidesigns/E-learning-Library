dbce96b79e14989883f88667334969a62b6a0f6f
Rechidesigns <alerechiordu@gmail.com>
2023-01-05 15:33:34 +0100

    URL

--- elearning/urls.py
+++ elearning/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path, include
-from .views import CategoryView, BookView
-from rest_framework import routers
+from . import views
 
-
-router = routers.DefaultRouter()
-router.register(r'category', CategoryView)
-router.register(r'book', BookView)
 
 urlpatterns = [
-    path('/', include(router.urls))
+    path('/', include('djoser.urls')),
 ]
 
